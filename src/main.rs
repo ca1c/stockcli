0603e4f46d956f2437a75379c80790a135cb9c4e
ca1c <gegs921@gmail.com>
2024-06-10 23:07:11 -0700

    parsing json into a struct

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,36 @@ use std::env;
 use std::collections::HashMap;
 use anyhow::{Result};
 use clap::Parser;
-use ureq::{Error};
+// use ureq::{Error};
+use serde::{Deserialize, Serialize};
+use serde_json::Result as SerdeResult;
 
 
 #[derive(Parser)]
 struct Cli {
     symbol: String,
+}
+
+/*
+* c: current price
+* d: change
+* dp: percent change
+* h: high price of the day
+* l: low price of the day
+* o: open price of the day
+* pc: previous close price
+*/
+
+#[derive(Serialize, Deserialize)]
+struct StockQuote {
+    symbol: String,
+    c: f32,
+    d: f32,
+    dp: f32,
+    h: f32,
+    l: f32,
+    o: f32,
+    pc: f32,
 }
 
 fn main() {
@@ -40,7 +64,7 @@ fn init_env_vars() -> HashMap<String, String> {
     env_vars
 }
 
-fn get_stock_info(env_vars: &HashMap<String, String>, symbol: &String) -> Result<String, Error> {
+fn get_stock_info(env_vars: &HashMap<String, String>, symbol: &String) -> Result<String, Box<dyn std::error::Error>> {
     let token_value = env_vars.get("API_KEY").unwrap();
 
     let url = format!("https://finnhub.io/api/v1/quote?symbol={}&token={}", symbol, token_value);
@@ -48,7 +72,25 @@ fn get_stock_info(env_vars: &HashMap<String, String>, symbol: &String) -> Result
     let response: String = ureq::get(url.as_str())
         .call()?
         .into_string()?;
+    
+    let quote = parse_response(&response, symbol)?;
+
+    println!("quote: {}", quote.c);
 
     Ok(response)
+}
+
+fn parse_response(json_response: &String, symbol: &String) -> SerdeResult<StockQuote> {
+    let mut copy_json_response = (*json_response).clone();
+    copy_json_response.remove(0);
+
+    let combined_json_response = format!("{{\"symbol\":\"{}\",{}", (*symbol).clone(), copy_json_response);
+
+    println!("{}", combined_json_response);
+
+    let parsed_response: StockQuote = serde_json::from_str(combined_json_response.as_str())?;
+    println!("{}", parsed_response.c);
+
+    Ok(parsed_response)
 }
 
