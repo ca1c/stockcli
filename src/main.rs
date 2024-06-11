e27992edbb8c2ddd3966dec9369854b250bf9e27
ca1c <gegs921@gmail.com>
2024-06-10 20:03:13 -0700

    basic stock data into stdout

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::env;
 use std::collections::HashMap;
 use anyhow::{Result};
 use clap::Parser;
+use ureq::{Error};
 
 
 #[derive(Parser)]
@@ -12,13 +13,21 @@ struct Cli {
     symbol: String,
 }
 
-fn main() -> Result<()> {
+fn main() {
     dotenv().ok();
 
     let args = Cli::parse();
     let env_vars = init_env_vars();
 
-    Ok(())
+    let stock_info = match get_stock_info(&env_vars, &args.symbol) {
+        Ok(data) => data,
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            return;
+        }
+    };
+
+    println!("{}", stock_info);
 }
 
 fn init_env_vars() -> HashMap<String, String> {
@@ -29,5 +38,17 @@ fn init_env_vars() -> HashMap<String, String> {
     }
 
     env_vars
+}
+
+fn get_stock_info(env_vars: &HashMap<String, String>, symbol: &String) -> Result<String, Error> {
+    let token_value = env_vars.get("API_KEY").unwrap();
+
+    let url = format!("https://finnhub.io/api/v1/quote?symbol={}&token={}", symbol, token_value);
+
+    let response: String = ureq::get(url.as_str())
+        .call()?
+        .into_string()?;
+
+    Ok(response)
 }
 
